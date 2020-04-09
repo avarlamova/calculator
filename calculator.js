3b37ef232332213c6f0217efb8c511ff4ccc9bdc
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-04-09 23:33:17 +0300

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,13 @@
+﻿
+function insert(number) {
+    $('#display').val($('#display').val() + number);
+}
+
+function calculate() {
+    $('#display').val(eval($('#display').val()))
+}
+
+$("#delete-all").on('click', function () {
+    $('#display').val('')
+})
+
