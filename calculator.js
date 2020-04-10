e56f19b571d6f59ec789d3f181bf18b9ff0a0366
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-04-10 20:51:50 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -3,11 +3,16 @@ function insert(number) {
     $('#display').val($('#display').val() + number);
 }
 
-function calculate() {
+$('#calculate').on('click', function () {
     $('#display').val(eval($('#display').val()))
 }
+    )
 
 $("#delete-all").on('click', function () {
     $('#display').val('')
 })
 
+$("#delete").on('click', function () {
+    value = $('#display').val();
+    $('#display').val(value.substring(0, value.length - 1));
+})
