1090ba9c65f91368985f1fd6ee602ced4192efd6
avarlamova <59831804+avarlamova@users.noreply.github.com>
2020-04-14 22:39:05 +0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -35,3 +35,99 @@ $('#day-mode').on('click', function () {
         $('#day-mode').hide();
         $('#night-mode').show();
     })
+
+
+$(window).keydown(function (e) {
+	switch (e.which) {
+		case 48:
+			insert(0)
+			break;
+		case 49:
+			insert(1)
+			break;
+		case 50:
+			insert(2)
+			break;
+		case 51:
+			insert(3)
+			break;
+		case 52:
+			insert(4)
+			break;
+		case 53:
+			insert(5)
+			break;
+		case 54:
+			insert(6)
+			break;
+		case 55:
+			insert(7)
+			break;
+		case 56:
+			insert(8)
+			break;
+		case 57:
+			insert(9)
+			break;
+		case 191:
+			insert('+')
+			break;
+		case 190:
+			insert('.')
+			break;
+		case 88:
+			insert('*')
+			break;
+		case 189:
+			insert("-")
+			break;
+		case 187:
+			insert('+')
+			break;
+		case 96:
+			insert(0)
+			break;
+		case 97:
+			insert(1)
+			break;
+		case 98:
+			insert(2)
+			break;
+		case 99:
+			insert(3)
+			break;
+		case 100:
+			insert(4)
+			break;
+		case 101:
+			insert(5)
+			break;
+		case 102:
+			insert(6)
+			break;
+		case 103:
+			insert(7)
+			break;
+		case 104:
+			insert(8)
+			break;
+		case 105:
+			insert(9)
+			break;
+		case 106:
+			insert("*")
+			break;
+		case 107:
+			insert("+")
+			break;
+		case 13:
+			calc()
+			break;
+		case 110:
+			insert(".")
+			break;
+		default:
+			return false;
+	}
+}
+)
